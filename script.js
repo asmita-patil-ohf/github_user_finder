9e95c2f9a195c26f2418269ca8711d28315f304c
Asmita Patil <1646asmitapatil@gmail.com>
2026-02-24 23:44:43 +0530

    Update CSS code

--- script.js
+++ script.js
@@ -16,19 +16,41 @@ async function findUser() {
         }
 
         const data = await response.json();
-
         document.getElementById("result").innerHTML = `
-            <img src="${data.avatar_url}" width="100">
-            <h2>${data.name}</h2>
-            <p>Followers: ${data.followers}</p>
-            <a href="${data.html_url}" target="_blank">View Profile</a>
-        `;
+    <div class="profile-card">
+        <div class="left_profile">
+            <img src="${data.avatar_url}" class="avatar">
+        </div>
+     <div class="right_profile">
+        <h2 class="name">${data.name || "No name available"}</h2>
+
+        <div class="stats">
+            <div>
+                <h3>${data.public_repos}</h3>
+                <p>Repos</p>
+            </div>
+
+            <div>
+                <h3>${data.followers}</h3>
+                <p>Followers</p>
+            </div>
+        </div>
+
+        <a href="${data.html_url}" target="_blank" class="profile-btn">
+            View Profile
+        </a>
+
+    </div>
+    </div>
+`;
 
     } catch (error) {
 
         document.getElementById("result").innerHTML =
-            `<p style="color:red;">User not found</p>`;
-
+            `<div class="not_found">
+            <p>User not found!</p>
+            </div>
+            `;
     }
 
 }
